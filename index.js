ccd4180e09bd93c07af6bd04e8eaa2db673ed915
Rogelio Martinez <rogejr89@gmail.com>
2024-09-17 18:38:14 -0400

    completed block 20 workshop

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 // function to get all numbers. only numbers. 
 
 const numbers = [];
-const odd = [];
-const even = [];
+const evenNums = [];
+const oddNums = [];
 
-function addNumber(num) {
+function addNumber2Arr(num) {
     numbers.push(num)
 }
 
+function sortEvenOdd(){
+    const num = numbers.shift();
+    if (num%2 === 0) {
+        evenNums.push(num)
+    } else {
+        oddNums.push(num);
+    }
+}
 
+// render function to add to bank
 function renderNumberBank() {
     const numsInBank = numbers.map((number) => {
         const p = document.createElement("p");
@@ -20,15 +29,65 @@ function renderNumberBank() {
     numberBank.replaceChildren(...numsInBank);
 }
 
-function render() {
-    renderNumberBank();
+//render function to even arr
+function renderEvenNums() {
+    const evens = evenNums.map((number) => {
+        const p = document.createElement("p");
+        p.textContent = number;
+        return p;
+    });
+    const evenNumsElement = document.querySelector("#evens output");
+    evenNumsElement.replaceChildren(...evens);
 }
 
-//set up event listener and add number to numbers array.
+//render function to odd arr
+function renderOddNums() {
+    const odds = oddNums.map((number) => {
+        const p = document.createElement("p");
+        p.textContent = number;
+        return p;
+    });
+    const oddNumsElement = document.querySelector("#odds output");
+    oddNumsElement.replaceChildren(...odds);
+}
+
+//main render function to call all functions
+function render() {
+    renderNumberBank();
+    renderEvenNums();
+    renderOddNums();
+}
+
+//event listener form.
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const numberInput = document.querySelector("#number");
-    addNumber(numberInput.value);
+    addNumber2Arr(numberInput.value);
     render();
 });
+
+//event listener #sortOne button.
+const sortOne = document.querySelector("#sortOne");
+sortOne.addEventListener("click", () => {
+    sortEvenOdd();
+    render()
+});
+
+//event listener #sortAll button.
+const sortAll = document.querySelector("#sortAll");
+sortAll.addEventListener("click", () => {
+    while (numbers.length > 0) {
+        sortEvenOdd();
+    }
+    render() 
+});
+
+
+
+
+
+
+
+
+
